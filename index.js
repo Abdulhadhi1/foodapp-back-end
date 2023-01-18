e9daf7476661277371a35d623ad19e89867e117c
Abdul Hadhi <108865426+Abdulhadhi1@users.noreply.github.com>
2023-01-18 11:48:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+const dotenv = require("dotenv");
+
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+
+const orderRouter = require("./routes/orderRoutes.js");
+const pizzaRouter =require( "./routes/pizzasRoutes.js");
+const userRouter = require("./routes/userRoutes.js");
+const app = express();
+dotenv.config();
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+    res.send("Server Working 👍🎊🎊");
+});
+app.use('/api/pizzas', pizzaRouter);
+app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
+
+mongoose
+  .connect("mongodb+srv://abdul:abdul123@cluster0.qlddgq6.mongodb.net/?retryWrites=true&w=majority")
+  .then(() => {
+    console.log("Connected To DataBase");
+  })
+  .catch(() => {
+    console.log("Error While Connecting the database");
+  });
+
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server Running Successfully on PORT ${PORT}`));
